fcbddc6af4e72d69883ebd43f45398ef2790e5ea
Basil Kraft <29065166+bask0@users.noreply.github.com>
2024-12-30 17:50:26 +0100

    create file

---
+++ code/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sa2',
+    version='0.0.0',
+    description='',
+    author='Julian Kraft',
+    author_email='',
+    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
+    url='https://github.com/juliankraft/TermPaper2_RasterClassification',
+    packages=find_packages(),
+)
