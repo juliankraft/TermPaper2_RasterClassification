616641d80af0fd9a6d8f8b4c771b800a7a54ce70
Basil Kraft <29065166+bask0@users.noreply.github.com>
2024-12-31 14:46:36 +0100

    ad lightning routine

--- code/run_model.py
+++ code/run_model.py
@@ -1,93 +1,143 @@
 
-from src.dataloader import AugmentorChain
+import os
+import shutil
+from pathlib import Path
+from argparse import ArgumentParser, Namespace
+import lightning as L
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
+from lightning.pytorch.loggers import TensorBoardLogger
+from typing import cast
+
+from src.dataloader import RSDataModule
+from src.augmentors import AugmentorChain
+from src.model import LightningResNet
+from src.utils import PredictionWriter
+
+
+def make_dir_from_args(base_path: Path | str, args: Namespace) -> Path:
+    base_path = Path(base_path)
+    specific_path = 'augment' if args.use_data_augmentation else 'noaugment'
+    path = base_path / specific_path
+
+    if path.exists():
+        if args.overwrite:
+            shutil.rmtree(path)
+        else:
+            raise FileExistsError(
+                'Base directory exists, but overwrite is False. '
+                f'Use `-o` or `--overwrite` to delete existing runs in `{path}`.'
+            )
+    else:
+        os.makedirs(path)
+
+    return path
 
 
 if __name__ == '__main__':
 
-    ac = AugmentorChain(
-        random_seed=1,
-        augmentors=[
-            FlipAugmentor(),
-            RotateAugmentor(),
-            PixelNoiseAugmentor(scale=0.1),
-            ChannelNoiseAugmentor(scale=0.1)
-        ]
-    )
+    # Initialize the argument parser
+    parser = ArgumentParser()
 
-    rsdata_train = RSData(
-        './data/combined.zarr',
-        mask_area=[1],
-        cutout_size=21,
-        augmentor_chain=ac
-    )
+    # Add arguments to the parser
+    parser.add_argument(
+        '--device', type=str, default='cpu',
+        help='Device to run the model on (cuda or cpu).')
+    parser.add_argument(
+        '--num_classes', type=int, default=10,
+        help='Number of classes in the dataset.')
+    parser.add_argument(
+        '--batch_size', type=int, default=16,
+        help='Batch size for training.')
+    parser.add_argument(
+        '--num_workers', type=int, default=0,
+        help='The number of dataloader workers.')
+    parser.add_argument(
+        '--cutout_size', type=int, default=21,
+        help='Cutout size in pixels of the input feature image.')
+    parser.add_argument(
+        '--learning_rate', type=float, default=0.01,
+        help='Learning rate for the optimizer.')
+    parser.add_argument(
+        '--weight_decay', type=float, default=0.0,
+        help='Weight decay for the optimizer.')
+    parser.add_argument(
+        '--use_data_augmentation', action='store_true',
+        help='Flag to enable data augmentation.')
+    parser.add_argument(
+        '--patience', type=int, default=5,
+        help='The early stopping patience; how many epochs the validation loss can stagnate before stopping.')
+    parser.add_argument(
+        '-o', '--overwrite', action='store_true',
+        help='Overwrite existing runs. Default is False and an error is thrown if directory exists.')
+    parser.add_argument(
+        '--dev_run', action='store_true',
+        help='Runs 3 epochs with one batch of training and validation each.')
 
-    rsdata_valid = RSData(
-        './data/combined.zarr',
-        mask_area=[2],
-        cutout_size=21,
+    # Parse the arguments
+    args = parser.parse_args()
+
+    if args.use_data_augmentation:
+        ac = AugmentorChain.from_args(
+            'FlipAugmentor',
+            'RotateAugmentor',
+            random_seed=1,
+        )
+    else:
+        ac = None
+
+    datamodule = RSDataModule(
+        ds_path='../data/Sample_CombinedData_32signed/combined.zarr',
+        train_area_ids=[1, 2],
+        valid_area_ids=[3],
+        test_area_ids=[4],
+        cutout_size=args.cutout_size,
         augmentor_chain=ac,
-        rs_means=rsdata_train.rs_means,
-        rs_stds=rsdata_train.rs_stds
+        batch_size=args.batch_size,
+        num_workers=args.num_workers
     )
-
-    print('Creating dataloaders...')
-
-    train_dl = DataLoader(rsdata_train, batch_size=32, shuffle=True, num_workers=6)
-    valid_dl = DataLoader(rsdata_valid, batch_size=32, shuffle=False, num_workers=6)
-
-    print('Creating dataloaders done.')
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print('device:', device)
-    num_classes = 10
-    num_epochs = 2
-    # batch_size = 16
-    learning_rate = 0.01
 
     # Model
-    model = ResNet(inplanes=4, num_classes=num_classes).to(device)
-    print('Number of trainable parameters: ', count_trainable_parameters(model))
+    model = LightningResNet(
+        num_classes=args.num_classes,
+        learning_rate=args.learning_rate,
+        weight_decay=args.weight_decay
+    )
 
-    # Loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=0.000, momentum=0.9)
+    logdir = make_dir_from_args(base_path='../runs', args=args)
 
-    total_step = len(train_dl)
+    # For dev run, limit batches and max_epochs.
+    dev_run_args = {
+        'limit_train_batches': 1 if args.dev_run else 0.1,  # (float = fraction, int = num_batches)
+        'limit_val_batches': 1 if args.dev_run else 1.0,
+        'limit_test_batches': 1 if args.dev_run else 1.0,
+        'limit_predict_batches': 200 if args.dev_run else 1.0,
+        'max_epochs': 3 if args.dev_run else 300,
+        'log_every_n_steps': 1 if args.dev_run else 10,
+    }
 
-    for epoch in range(num_epochs):
-        for i, (images, labels) in tqdm(
-            enumerate(train_dl),
-            total=total_step,
-            desc=f'Epoch {epoch+1}/{num_epochs} (train)'
-        ):
-            # Move tensors to the configured device
-            images = images.to(device)
-            labels = labels.to(device)
+    trainer = L.Trainer(
+        default_root_dir=logdir,
+        accelerator=args.device,
+        callbacks=[
+            ModelCheckpoint(
+                filename='best',
+                save_last=True,
+                save_top_k=1,
+                every_n_epochs=1
+            ),
+            EarlyStopping(
+                monitor='valid_loss',
+                patience=args.patience),
+            PredictionWriter(output_dir=logdir),
+        ],
+        logger=TensorBoardLogger(
+            save_dir=logdir,
+            version='',
+            name=''),
+        **dev_run_args
+    )
 
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+    trainer.fit(model=model, datamodule=datamodule)
 
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            del images, labels, outputs
-            torch.cuda.empty_cache()
-
-        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
-
-        # Validation
-        with torch.no_grad():
-            correct = 0
-            total = 0
-            for images, labels in valid_dl:
-                images = images.to(device)
-                labels = labels.to(device)
-                outputs = model(images)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-                del images, labels, outputs
-
-            print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
+    best_model_path = cast(str, trainer.checkpoint_callback.best_model_path)  # type: ignore
+    trainer.predict(model=model, datamodule=datamodule, ckpt_path=best_model_path)
