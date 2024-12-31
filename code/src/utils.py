d69df3217aa1855dc9a7613944561a1309ea29b7
Basil Kraft <29065166+bask0@users.noreply.github.com>
2024-12-31 14:26:03 +0100

    change prediction dtype to float16

---
+++ code/src/utils.py
@@ -0,0 +1,67 @@
+import xarray as xr
+import lightning as L
+from lightning.pytorch.callbacks import BasePredictionWriter
+from pathlib import Path
+import numpy as np
+import os
+
+from torch.utils.data import DataLoader
+from torch import Tensor
+from typing import Any, Sequence, cast
+
+from src.model import LightningResNet
+
+
+class PredictionWriter(BasePredictionWriter):
+    def __init__(self, output_dir: str | Path):
+        super().__init__(write_interval='batch')
+
+        self.output_dir = output_dir
+        os.makedirs(output_dir, exist_ok=True)
+
+    def on_predict_start(
+            self,
+            trainer: L.Trainer,
+            pl_module: LightningResNet) -> None:
+
+        predict_dataloader = cast(DataLoader, trainer.predict_dataloaders)
+        ds = cast(xr.Dataset, predict_dataloader.dataset.ds)  # type: ignore
+        num_classes = pl_module.num_classes
+
+        self.mask = ds.mask
+        self.da = xr.full_like(
+            ds['label'].load().expand_dims(cls=np.arange(num_classes), axis=-1),
+            dtype=np.float16,
+            fill_value=np.nan)
+        self.da.name = 'label_pred'
+
+    def write_on_batch_end(
+            self,
+            trainer: L.Trainer,
+            pl_module: L.LightningModule,
+            prediction: tuple[Tensor, dict[str, Tensor]],
+            batch_indices: Sequence[int] | None,
+            batch: Any,
+            batch_idx: int,
+            dataloader_idx: int) -> None:
+
+        predicted_label, xy_i = prediction
+
+        predicted_label = predicted_label.cpu()
+        xi = xy_i['xi'].cpu()
+        yi = xy_i['yi'].cpu()
+
+        for p, x, y in zip(predicted_label, xi, yi):
+            self.da[{'x': x, 'y': y}] = p
+
+    def on_predict_end(self, trainer: L.Trainer, pl_module: L.LightningModule) -> None:
+        ds = xr.Dataset({
+            'label_pred': self.da,
+            'training_mask': self.mask
+        })
+        save_path = self.make_predition_path(self.output_dir)
+        ds.to_zarr(save_path, mode='w')
+
+    @staticmethod
+    def make_predition_path(output_dir: str | Path) -> str:
+        return os.path.join(output_dir, 'preds.zarr')
