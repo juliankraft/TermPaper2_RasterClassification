8d89839f9a1014d3be95a0a5bb589e742e0c79a2
juliankraft <kraftjulian@gmail.com>
2025-01-08 11:11:21 +0100

    add mail

--- code/setup.py
+++ code/setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.0.0',
     description='',
     author='Julian Kraft',
-    author_email='',
+    author_email='kraftjul@students.zhaw.ch',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     url='https://github.com/juliankraft/TermPaper2_RasterClassification',
     packages=find_packages(),
